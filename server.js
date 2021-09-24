b06c024b791d427fd879d310ac77d867c0f661dc
Damian De Leon <damianmdeleon@gmail.com>
2021-09-24 10:33:13 -0500

    udpated code

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const morgan = require('morgan'); 
 
-
+const PORT = 3030;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -16,18 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// routes
-equire("./routes/api.js")(app);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", 
-{
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false
-})
+});
 
-
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
